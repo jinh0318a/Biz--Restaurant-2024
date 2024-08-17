ea5357e2acba0728ca10aaae4b2823b59952a7d6
jinh0318 <jinh0318a@naver.com>
2024-08-17 20:16:06 +0900

    all

--- FineDust/src/main/java/com/callor/finedust/controller/HomeController.java
+++ FineDust/src/main/java/com/callor/finedust/controller/HomeController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.callor.finedust.model.Finedust;
 import com.callor.finedust.service.FineDustService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Handles requests for the application home page.
@@ -30,6 +33,20 @@ public class HomeController {
 		model.addAttribute("FINEDUST_LIST", finedustList);
 
 		return "list";
+	}
+
+	@GetMapping("/detail")
+	public String detail(Model model) throws JsonProcessingException {
+		List<Finedust> finedustList = fineDustService.lastData();
+
+		// Convert list to JSON string
+		ObjectMapper objectMapper = new ObjectMapper();
+		String finedustListJson = objectMapper.writeValueAsString(finedustList);
+
+		// Add JSON string to model
+		model.addAttribute("finedustList", finedustListJson);
+
+		return "detail";
 	}
 
 }
