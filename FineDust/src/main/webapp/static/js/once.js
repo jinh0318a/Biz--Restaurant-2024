c824cc884b8a51b8a80cb912b950c259ab7ac3b7
jinh0318a <jinh0318a@naver.com>
2024-08-20 15:41:36 +0900

    once.css

--- FineDust/src/main/webapp/static/js/once.js
+++ FineDust/src/main/webapp/static/js/once.js
@@ -1,12 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const PM10 = document.querySelector("td.PM10");
-  const PM25 = document.querySelector("td.PM10");
+  const PM25 = document.querySelector("td.PM25");
 
   const applyColorPM10 = (PM10) => {
-    const PM10 = parseFloat(PM10.textContent);
+    const PM10_value = parseFloat(PM10.textContent);
+    console.log(PM10_value);
+    if (PM10_value >= 151) {
+      PM10.style.backgroundColor = "red";
+    } else if (PM10_value >= 81 && PM10_value <= 150) {
+      PM10.style.backgroundColor = "yellow";
+    } else if (PM10_value >= 31 && PM10_value <= 80) {
+      PM10.style.backgroundColor = "green";
+    } else if (PM10_value <= 30) {
+      PM10.style.backgroundColor = "blue";
+    }
   };
 
   const applyColorPM25 = (PM25) => {
-    const PM25 = parseFloat(PM25.textContent);
+    const PM25_value = parseFloat(PM25.textContent);
+    if (PM25_value >= 76) {
+      PM25.style.backgroundColor = "red";
+    } else if (PM25_value >= 36 && PM25_value <= 75) {
+      PM25.style.backgroundColor = "yellow";
+    } else if (PM25_value >= 16 && PM25_value <= 35) {
+      PM25.style.backgroundColor = "green";
+    } else if (PM25_value <= 15) {
+      PM25.style.backgroundColor = "blue";
+    }
   };
+
+  applyColorPM10(PM10);
+  applyColorPM25(PM25);
 });
